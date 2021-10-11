22cf0ce8b0e4c0cc3d86531a8c6d5681f8d3965c
Piotr Stolarczyk <piotr@immotef.com>
2021-10-12 01:46:25 +0200

    fix build

--- src/Core/Controller/EventDispatcher.d.ts
+++
@@ -1,9 +0,0 @@
-import { Event, Callback } from './types';
-declare class EventDispatcher {
-    private eventMap;
-    subscribe(eventName: string, callback: Callback): void;
-    unsubscribe(eventName: string, callback: Callback): void;
-    emit(eventName: string, event: Event): void;
-    clear(): void;
-}
-export default EventDispatcher;
--- src/Core/Controller/types.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type Event = {
-    type: string;
-    data: any;
-};
-export declare type Callback = (event: Event) => void;
